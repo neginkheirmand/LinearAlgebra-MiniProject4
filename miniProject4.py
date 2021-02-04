5c888a9ec072b09ed64bc1a5c1b5074555fd3c04
neginkheirmand <neginkheirmand@gmail.com>
2021-02-04 10:49:03 +0330

    working version of the code

--- miniProject4.py
+++ miniProject4.py
@@ -14,6 +14,9 @@ def readFile():
     if img is None:
         sys.exit("Could not read the image.")
         return False
+    cv.imshow("Noisy -starting- Image", img)
+    print("To continue press any key")
+    k = cv.waitKey(0)
     return True
     
 #the rgb value is the index of the channel under process
@@ -30,21 +33,32 @@ def process(k, rgbChannel ):
     #we now that the number of non zero diagonal values are r and r is less than/equal to the min of height and wifth 
     for i in range(0, r):
         if i<=k:
-            R[i][i]=S[i][i]
+            R[i][i]=S[i]
         else:
             break
     output = np.dot(U, R)
     output = np.dot(output, V)
     return output    
         
-def saveFile():
-    global img
-    cv.imshow("Display window", img)
-    print("press s key to save the file and close the window")
+def saveFile(matrix):
+    print("press s key to save the file, if you dont want to save it press any other key")
+    cv.imshow("Final Denoised Version of the image", matrix)
     k = cv.waitKey(0)
     if k == ord("s"):
         print("the image was save as clean.jpg")
-        cv.imwrite("clean.jpg", img)
+        
+        cv.imwrite("newClean.jpg", matrix)
+        exit()
+    else: 
+        print("Was not saved")
+        exit()
+
+def putInMatrix(newChannels, newMatx, channel):
+    height, width = newMatx.shape
+    for i in range(0, height):
+        for j in range(0, width):
+            newChannels[i][j][channel]= newMatx[i][j]
+    return
 
 def main():
     global img
@@ -57,23 +71,16 @@ def main():
             except ValueError:
                 print("Please enter a number")
                 continue
-        cv.imshow("Noisy -starting- Image", img)
-        s = cv.waitKey(0)
         height, width, numChannels = img.shape
         newChannels = np.zeros((height, width, numChannels))
         for i in range( 0, numChannels):
             newMatx = process(k, i)
-            np.add(newChannels, newMatx)
+            putInMatrix(newChannels, newMatx, i)
+            cv.imshow("new Channel Dinoised", newChannels)
+            key = cv.waitKey(0)
+        saveFile(newChannels)
     else:
         return
 
 
-# main()
-
-A = np.array([[4, 11, 14], [8, 7, 2]])
-U, S, V = np.linalg.svd(A)
-print(U)
-print()
-print(S)
-print()
-print(V)
+main()
