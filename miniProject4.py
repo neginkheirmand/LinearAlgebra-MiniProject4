f06cd6786fb045445f8faae159b785c6e18afbc1
neginkheirmand <neginkheirmand@gmail.com>
2021-02-04 11:48:21 +0330

    debug

--- miniProject4.py
+++ miniProject4.py
@@ -2,14 +2,13 @@ import cv2 as cv
 import sys
 import numpy as np
 
+
 img = None
-U=None
-S=None 
-V= None
+
 def readFile():
     global img
-    #if you change the input image, rememmber to chaneg the name of file in the line bellow 
-    #change the file format(.png .jpg .bmp ...) of the output file -in method saveFile()- accordingly
+    #if you change the input image, rememmber to changed the name of file in the line bellow 
+    #change the file format(.png .jpg .bmp ...) of the output file -in method saveImg()- accordingly
     img = cv.imread("noisy.jpg",1)
     if img is None:
         sys.exit("Could not read the image.")
@@ -23,10 +22,10 @@ def readFile():
 def process(k, rgbChannel ):
     #U*R*Vᵗ = Cleared
     #U*S*Vᵗ = Noisy_Matrix
-    global U, S, V
     global img
     imageChannel = img[:,:,rgbChannel]
     U, S, V = np.linalg.svd(imageChannel)
+    #Taking SVD computation as A= U D (V^T), For U, D, V = np.linalg.svd(A), this function returns V in Vᵗ form already.
     height, width = imageChannel.shape
     R = np.zeros((height, width))
     r = min(height, width)
@@ -39,30 +38,31 @@ def process(k, rgbChannel ):
     output = np.dot(U, R)
     output = np.dot(output, V)
     return output    
-        
-def saveFile(matrix):
+    
+def saveImg(newChannels):
     print("press s key to save the file, if you dont want to save it press any other key")
-    cv.imshow("Final Denoised Version of the image", matrix)
+    cv.imshow("Final Denoised Version of the image", newChannels)
     k = cv.waitKey(0)
     if k == ord("s"):
-        print("the image was save as clean.jpg")
-        
-        cv.imwrite("newClean.jpg", matrix)
-        exit()
+        cv.imwrite("newClean.jpg", newChannels)
+        print("the image was saved as clean.jpg")
+        return
     else: 
         print("Was not saved")
-        exit()
+        return
 
 def putInMatrix(newChannels, newMatx, channel):
     height, width = newMatx.shape
     for i in range(0, height):
         for j in range(0, width):
-            newChannels[i][j][channel]= newMatx[i][j]
+            newChannels[i][j][channel]= int(newMatx[i][j])
     return
+
 
 def main():
     global img
     if readFile():
+        #get input form user
         k = -1
         while True :
             try:
@@ -71,14 +71,21 @@ def main():
             except ValueError:
                 print("Please enter a number")
                 continue
+
         height, width, numChannels = img.shape
-        newChannels = np.zeros((height, width, numChannels))
+        newChannels = np.zeros((height, width, numChannels), dtype=np.uint8)
         for i in range( 0, numChannels):
             newMatx = process(k, i)
+            
             putInMatrix(newChannels, newMatx, i)
-            cv.imshow("new Channel Dinoised", newChannels)
-            key = cv.waitKey(0)
-        saveFile(newChannels)
+            #you can also use the line above to put the output image into each channel but the method putInMatrix has casting integrated
+            # so its preferable to use the method putInMatrix  
+            # newChannels[:,:,i]=newMatx
+
+            # if you want to see the process of denoising each of the channels, un-comment the following lines
+            # cv.imshow("new Channel Dinoised", newChannels)
+            # key = cv.waitKey(0)
+        saveImg(newChannels)
     else:
         return
 
